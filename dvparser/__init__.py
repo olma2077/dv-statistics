68a5dac224f46b8187ae29ff4deb77c7de791463
Oleg Matviichuk <ma7hway@gmail.com>
2024-11-19 12:13:48 +0100

    chore: remove unnecessary file

--- dvparser/__init__.py
+++
@@ -1,9 +0,0 @@
-"""dv-parser package implements DV reports parsing application.
-
-Parser is capable to parse 3 types of files:
-* Number of online entries
-* Number of selected entrants
-* Number of visa issuances
-
-All files are available from travel.state.gov: https://bit.ly/2LaF5Z0
-"""
